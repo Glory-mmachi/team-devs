06ad33b6cf539363f9d2179af6c74cee2d9fb4ad
Okechukwu Chidera <okechukwuchidera12@gmail.com>
2023-08-27 17:46:44 +0100

    hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Header/>
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Header.module.css";
+import header from "../assets/Illustration-Header.png";
+
+export default function Header() {
+  return (
+    <header>
+      <div className={styles.hero}>
+        <p className={styles.discount}>
+          <span>75% save</span> For the black friday weekend
+        </p>
+        <h2>Fastest & secure platform to invest in crypto</h2>
+        <p className={styles.heroDetails}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus debitis
+          dicta officia adipisci porro molestiae recusandae id nam! Hic, minima.
+        </p>
+        <div className={`btn btn_accent ${styles.btnHeader}`}>
+          Try for free <span>&gt</span>
+        </div>
+      </div>
+      <img src={header} alt="" />
+    </header>
+  );
+}
